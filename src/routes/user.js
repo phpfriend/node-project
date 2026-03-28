52f1aaae0d10107397f0e4f7e1b194445b487ff8
Yogesh <ytiwari01@gmail.com>
2026-03-28 12:25:26 +0530

    created feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("./models/user");
 
 const userRouter = express.Router();
 
@@ -29,11 +30,53 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         { toUserId: loggedInUser._id, status: "accepted" },
         { fromUserid: loggedInUser._id, status: "accpted" },
       ],
-    }).populate("fromUserId", ["firstName", "lastName"]);
+    })
+      .populate("fromUserId", ["firstName", "lastName"])
+      .populate("toUserId", ["firstName", "lastName"]);
 
-    const data = connectionRequests.map((row) => row.fromUserId);
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
 
     res.json({ data });
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = pasrseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.page) || 10;
+    limit = limit > 50 ? 50 : limit;
+
+    const skip = (page - 1) * limit;
+
+    const connectionRequests = await ConectionRequest({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    })
+      .select("fromUserId", "toUserId", "age", "gender")
+      .skip(skip)
+      .limit(limit);
+
+    const hideUserFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUserFromFeed.add(req.fromUserId.toString());
+      hideUserFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUserFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select("fromUserId", "toUserId");
+    res.json({ data: users });
   } catch (err) {
     res.status(400).send({ message: err.message });
   }
