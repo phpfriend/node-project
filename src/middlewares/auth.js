f7f5bfadfe100132e527e0270b8cab777b7fcb32
Yogesh <ytiwari01@gmail.com>
2026-03-31 12:29:19 +0530

    fixed auth error

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,31 +1,29 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
-const userAuth = async(req, res, next) => {
-    try {
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
 
-        const { token } = req.cookies;
-        
-        if(!token){
-            throw new Error("Token is not valid!!!!!!");
-        }
-
-        const decodeObj = await jwt.verify(token, "DEV@Tinder$790"); 
-        const { _id } = decodeObj;
-        const user = await User.findById(_id);
-        console.log("User: "+ user);
-        if (!user){
-            throw new Error("User not found");
-        }
-
-        req.user = user;
-        next();
-
-    } catch(err) {
-        res.status(400).send("Error: "+err);
+    if (!token) {
+      return res.status(401).send("Please login!");
     }
+
+    const decodeObj = await jwt.verify(token, "DEV@Tinder$790");
+    const { _id } = decodeObj;
+    const user = await User.findById(_id);
+    // console.log("User: " + user);
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(400).send("Error: " + err);
+  }
 };
 
 module.exports = {
-    userAuth
-}; 
+  userAuth,
+};
