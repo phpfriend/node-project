c37a1a57904a12d6953288b0c5bce7f285eb81af
Yogesh <ytiwari01@gmail.com>
2026-04-02 22:59:57 +0530

    updated connection file and hide connection string

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,15 @@
-
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://ytiwari01_db_user:riJDXUaQbZmbE97z@namstecluster.qdmx6zg.mongodb.net/devTinder");
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connected");
+  } catch (err) {
+    console.error("Database connection failed", err);
+    process.exit(1);
+  }
 };
-
 module.exports = connectDB;
-
-
