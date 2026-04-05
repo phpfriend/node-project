84669e85f0772ed8d1c687011506c89aab3dc541
Yogesh <ytiwari01@gmail.com>
2026-04-06 00:03:57 +0530

    removed unused import

--- src/routes/request.js
+++ src/routes/request.js
@@ -3,7 +3,6 @@ const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const User = require("../models/user");
-const sendEmail = require("../utils/sendEmail");
 
 requestRouter.post(
   "/request/send/:status/:toUserId",
